7e2075d8751b5fc58bc3e268d6a83a9cb02b6399
Samir Rojas <samirrojasgamonal@gmail.com>
2026-05-20 15:32:18 -0500

    feat: grade input form component

--- src/routes/grades.js
+++ src/routes/grades.js
@@ -1,1 +1,11 @@
-console.log('Bienvenido a grades.js');
+// grade.js
+
+export const calculateAverage = (grades) => {
+    const total = grades.reduce((acc, grade) => acc + grade, 0);
+
+    return total / grades.length;
+};
+
+export const validateGrade = (grade) => {
+    return grade >= 0 && grade <= 20;
+};
